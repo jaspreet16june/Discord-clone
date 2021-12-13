b58d6c26154d5a5ebe521beedaac02c16c0257ed
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-12-13 16:32:37 +0530

    Sidebar Functionality

---
+++ discord-clone/src/component/Sidebar.jsx
@@ -0,0 +1,33 @@
+import ExpandMoreIcon from "@material-ui/icons/ExpandMoreOutlined";
+import AddIcon from "@material-ui/icons/AddOutlined";
+import React from "react";
+import "../css/sidebar.css"
+import SidebarChannel from "../component/SidebarChannel";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_Top">
+        <h3>Clever Programmar</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar_channels">
+        <div className="sidebar_channelsHeader">
+          <div className="sidebar_Header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar_addChannel" />
+        </div>
+        <div className="sidebarChannels_list">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ discord-clone/src/component/SidebarChannel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarChannel = () => {
+    return (
+        <div>
+            <h3>#AddChannel</h3>
+        </div>
+    )
+}
+
+export default SidebarChannel
